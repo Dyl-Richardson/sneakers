9bc17a48103a93d98027c0401f9fbdd79f5ed596
Léon FASHINGABO <53854@etu.he2b.be>
2021-10-11 11:33:19 +0200

    creating object sneaker

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const sneaker  = [{
+    name: 'Fall Limited Edition Sneakers',
+    mark: 'Sneaker Company',
+    men: true,
+    color: 'dark',
+    price: 125.00,
+    description: 'Those low profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole they will withstand everything the weathr can offer',
+},
+{
+    name: 'Fall Limited Edition Sneakers',
+    mark: 'Sneaker Company',
+    women: true,
+    color: 'light',
+    price: 125.00,
+    description: 'Those low profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole they will withstand everything the weathr can offer',
+}
+]
